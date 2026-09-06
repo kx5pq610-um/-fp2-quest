0a4a348f6cc0cae1e87979bdbf17f680f6f8208c
kx5pq610-um <kx5pq610@gmail.com>
2026-09-06 16:37:06 +0900

    Load Pack 14 and exam mode after 700-question generator

--- questions-pack12-merge.js
+++ questions-pack12-merge.js
@@ -1,2 +1,2 @@
 extraQuestions8.push(...extraQuestions12);
-document.write('<script src="questions-pack13.js"><\/script><script src="exam-mode.js"><\/script>');
+document.write('<script src="questions-pack13.js"><\/script><script src="questions-pack14.js"><\/script><script src="exam-mode.js"><\/script>');
