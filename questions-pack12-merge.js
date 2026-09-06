0a6995c2d518f91a6f42719ffc1d78e1e397db9c
kx5pq610-um <kx5pq610@gmail.com>
2026-09-06 16:31:05 +0900

    Load 700-question generator and exam modes

--- questions-pack12-merge.js
+++ questions-pack12-merge.js
@@ -1,2 +1,2 @@
 extraQuestions8.push(...extraQuestions12);
-document.write('<script src="questions-pack13.js"><\\/script>');
+document.write('<script src="questions-pack13.js"><\/script><script src="exam-mode.js"><\/script>');
